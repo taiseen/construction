e97f4086a67a245fb48d12c394d61bb3e128fa40
taiseen <taiseen.cse@gmail.com>
2022-02-18 08:59:34 +0600

    search + login ==> OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,36 @@
 // 18 - Feb - 2022
 
-const data = document.getElementById('data');
-const data = document.querySelector('.data');
+const menuBtn = document.getElementById('menu-btn');
+const searchBtn = document.getElementById('search-btn');
+const loginBtn = document.getElementById('login-btn');
 
+const navBar = document.querySelector('.navbar');
+const searchForm = document.querySelector('.search-form');
+const loginForm = document.querySelector('.login-form');
+
+
+
+menuBtn.addEventListener('click', () => {
+    navBar.classList.toggle('userClick');
+});
+
+searchBtn.addEventListener('click', () => {
+    searchForm.classList.toggle('userClick');
+    loginForm.classList.remove('userClick');
+});
+
+loginBtn.addEventListener('click', () => {
+    loginForm.classList.toggle('userClick');
+    searchForm.classList.remove('userClick');
+});
 
 window.addEventListener('scroll', () => {
-
-
-    if (window.scrollY > 80) {
-
-    } else {
-
-    }
-
+    navBar.classList.remove('userClick');
+    searchForm.classList.remove('userClick');
+    loginForm.classList.remove('userClick');
 });
+
+
 
 // swiper-slider functionality... 
 
